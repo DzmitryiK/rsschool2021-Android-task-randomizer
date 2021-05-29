c68e067d3bedcd6667a5e7f33dbb50f0ccfffd88
Dzmitryi Kukhmar <landos.dm@gmail.com>
2021-05-29 19:02:19 +0300

    feat: task complete

--- app/src/main/java/com/rsschool/android2021/FirstFragment.kt
+++ app/src/main/java/com/rsschool/android2021/FirstFragment.kt
@@ -1,17 +1,23 @@
 package com.rsschool.android2021
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
 
     private var generateButton: Button? = null
     private var previousResult: TextView? = null
+    private var intfListener: FragmentsInterface? = null
+    private var errorMes = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +25,11 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        intfListener = context as FragmentsInterface
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -29,12 +40,39 @@ class FirstFragment : Fragment() {
         val result = arguments?.getInt(PREVIOUS_RESULT_KEY)
         previousResult?.text = "Previous result: ${result.toString()}"
 
-        // TODO: val min = ...
-        // TODO: val max = ...
+        val etMin = view.findViewById<EditText>(R.id.min_value)
+        val etMax = view.findViewById<EditText>(R.id.max_value)
+
 
         generateButton?.setOnClickListener {
-            // TODO: send min and max to the SecondFragment
+            val min = checkEdit(etMin, "Min")
+            val max = checkEdit(etMax, "Max")
+
+            if (min != -1 && max != -1){
+                if (min > max){
+                    errorMes = "$errorMes\nMin is greater than Max"
+                }else intfListener?.onGenerate(min, max)
+            }
+            if (errorMes.isNotBlank()){
+                Toast.makeText(activity, errorMes.trim(), Toast.LENGTH_SHORT).show()
+            }
+            errorMes = ""
         }
+    }
+
+    private fun checkEdit(et:EditText, name:String):Int{
+        var res:Int?
+        if (et.text.isBlank()){
+            errorMes = "$errorMes\n$name is blank"
+            res = -1
+        }else{
+            res = et.text.toString().toIntOrNull()
+            if (res == null || res < 0){
+                errorMes = "$errorMes\nUnsupportable value in $name"
+                res = -1
+            }
+        }
+        return res
     }
 
     companion object {
